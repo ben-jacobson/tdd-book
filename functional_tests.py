a4f2da2e04d9d34653124693fb3a61bbdbe66d4f
Ben Jacobson <ben_jacobson@live.com>
2018-07-02 08:39:21 -1000

    Functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,8 +1,10 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from time import sleep
 import unittest
 
 class NewVistitorTest(unittest.TestCase):                   # inherits from unittest.TestCase which lays out the framework for unit testing
+
     def setUp(self):
         self.browser = webdriver.Firefox()
         sleep(1)                                            # encountering a bug wher making changes to the web driver too soon causes a pipe error
@@ -19,19 +21,35 @@ class NewVistitorTest(unittest.TestCase):                   # inherits from unit
         # She notices the page title and header mention to-do lists
         #assert 'To-Do' in browser.title, "Browser Title was '" + browser.title + "'"
         self.assertIn('To-Do', self.browser.title)          # assertIn equivalent of 'if x in y' or assert 'x' in y:
-        self.fail('Finish the test!')                       # at the moment just a reminder to finish the test
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         # She is invited to enter a to-do item straight away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
 
         # She types "Buy peacock feathers" into a text box (Edith's hobby
         # is tying fly-fishing lures)
+        inputbox.send_keys('Buy peacock feathers')
 
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list table
+        inputbox.send_keys(keys.ENTER)
+        sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)  # this is cool and new, works similar to a lambda function, pretty much like 'if x in array'
+        )
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very
         # methodical)
+        self.fail('Finish the test!')
 
         # The page updates again, and now shows both items on her list
 
