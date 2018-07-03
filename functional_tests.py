61f07e066aa63e307fb0d4d9dfd82697951c41c0
Ben Jacobson <ben_jacobson@live.com>
2018-07-03 10:19:47 -1000

    Continuing with functional testing, pushing a commit before a refactor

--- functional_tests.py
+++ functional_tests.py
@@ -1,13 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from time import sleep
+import time
 import unittest
 
 class NewVistitorTest(unittest.TestCase):                   # inherits from unittest.TestCase which lays out the framework for unit testing
 
     def setUp(self):
         self.browser = webdriver.Firefox()
-        sleep(1)                                            # encountering a bug wher making changes to the web driver too soon causes a pipe error
+        time.sleep(1)                                            # encountering a bug wher making changes to the web driver too soon causes a pipe error
         self.browser.set_page_load_timeout(30)
 
     def tearDown(self):                                     # setup and tearDown are inherited from unittest.TestCase and overwritten to perform specific functions that we control
@@ -38,21 +38,37 @@ class NewVistitorTest(unittest.TestCase):                   # inherits from unit
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list table
         inputbox.send_keys(Keys.ENTER)
-        sleep(1)
+        time.sleep(1)
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
+        '''self.assertTrue(
             any(row.text == '1: Buy peacock feathers' for row in rows),   # this is cool and new, works similar to a lambda function, pretty much like 'if x in array'
-            "New to-do item did not appear in table"
-        )
+            f"New to-do item did not appear in table. Contents were:\n{table.text}"
+        )'''
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very
         # methodical)
-        self.fail('Finish the test!')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
         # The page updates again, and now shows both items on her list
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn(
+            '2: Use peacock feathers to make a fly',
+            [row.text for row in rows]
+        )
+
+        # Edith wonders whether the site will remember her list. Then she sees
+        # that the site has generated a unique URL for her -- there is some 
+        # explanatory text to that effect.
+        self.fail('Finish the test!')
 
         # Satisfied, she goes back to sleep
 
