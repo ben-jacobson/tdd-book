238d56fc1319d15bf98496e5f367868dc97a6710
Ben Jacobson <ben_jacobson@live.com>
2018-07-02 14:27:21 -1000

    Front page HTML now generated from a template

--- functional_tests.py
+++ functional_tests.py
@@ -43,7 +43,8 @@ class NewVistitorTest(unittest.TestCase):                   # inherits from unit
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows)  # this is cool and new, works similar to a lambda function, pretty much like 'if x in array'
+            any(row.text == '1: Buy peacock feathers' for row in rows),   # this is cool and new, works similar to a lambda function, pretty much like 'if x in array'
+            "New to-do item did not appear in table"
         )
 
         # There is still a text box inviting her to add another item. She
