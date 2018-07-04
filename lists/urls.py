22e3ca0c43011e55498eda8f4390ded131108b81
Ben Jacobson <ben_jacobson@live.com>
2018-07-04 12:21:24 -1000

    Refactored url.py and created new url.py for lists app

---
+++ lists/urls.py
@@ -0,0 +1,11 @@
+
+from django.conf.urls import url
+from lists import views
+
+# in this project, by convention we are using urls without trailing slashes to indicate that this endpoint will modify the database in some way.
+
+urlpatterns = [
+    url(r'^new$', views.new_list, name='new_list'),
+    url(r'^(\d+)/$', views.view_list, name='view_list'),    
+    url(r'^(\d+)/add_item$', views.add_item, name='add_item'),    
+]
--- superlists/urls.py
+++ superlists/urls.py
@@ -13,17 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
-from lists import views
+from django.conf.urls import include, url
+from lists import views as list_views
+from lists import urls as list_urls
 #from django.contrib import admin
 
 # in this project, by convention we are using urls without trailing slashes to indicate that this endpoint will modify the database in some way.
 
 urlpatterns = [
     #url(r'^admin/', admin.site.urls),
-    url(r'^$', views.home_page, name='home'),
-    url(r'^lists/new$', views.new_list, name='new_list'),
-    #url(r'^lists/the-only-list-in-the-world/$', views.view_list, name='view_list'),
-    url(r'^lists/(\d+)/$', views.view_list, name='view_list'),    
-    url(r'^lists/(\d+)/add_item$', views.add_item, name='add_item'),    
+    url(r'^$', list_views.home_page, name='home'),
+    url(r'^lists/', include(list_urls)),    
 ]
