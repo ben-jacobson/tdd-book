bac0dba393412a3d9cfda4aeefd4be6cb7d48987
Ben Jacobson <ben_jacobson@live.com>
2018-07-03 18:33:57 -1000

    New URL view and template to display lists

--- lists/views.py
+++ lists/views.py
@@ -5,7 +5,9 @@ from lists.models import Item
 def home_page(request):
     if request.method == 'POST':
         Item.objects.create(text=request.POST['item_text'])
-        return redirect('/')  # always re-direct a POST request, https://en.wikipedia.org/wiki/Post/Redirect/Get
+        return redirect('/lists/the-only-list-in-the-world/')  # always re-direct a POST request, https://en.wikipedia.org/wiki/Post/Redirect/Get
+    return render(request, 'home.html')
 
+def view_list(request):
     items = Item.objects.all()
-    return render(request, 'home.html', {'items': items})
+    return render(request, 'list.html', {'items': items})
