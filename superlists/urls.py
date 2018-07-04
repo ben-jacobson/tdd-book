89232063ca2384963dbf2e4b7924de4ed13d9e62
Ben Jacobson <ben_jacobson@live.com>
2018-07-04 12:12:49 -1000

    new URL + view for adding to existing lists. FT passes!

--- superlists/urls.py
+++ superlists/urls.py
@@ -15,13 +15,15 @@ Including another URLconf
 """
 from django.conf.urls import url
 from lists import views
-from django.contrib import admin
+#from django.contrib import admin
 
 # in this project, by convention we are using urls without trailing slashes to indicate that this endpoint will modify the database in some way.
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    #url(r'^admin/', admin.site.urls),
     url(r'^$', views.home_page, name='home'),
     url(r'^lists/new$', views.new_list, name='new_list'),
-    url(r'^lists/the-only-list-in-the-world/$', views.view_list, name='view_list'),
+    #url(r'^lists/the-only-list-in-the-world/$', views.view_list, name='view_list'),
+    url(r'^lists/(\d+)/$', views.view_list, name='view_list'),    
+    url(r'^lists/(\d+)/add_item$', views.add_item, name='add_item'),    
 ]
