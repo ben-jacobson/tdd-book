139b0e9599bf0347122e16845c1ab6813659355f
ben-jacobson <ben_jacobson@live.com>
2018-07-07 20:08:27 -1000

    Add gunicorn to virtualenv requirements

--- superlists/settings.py
+++ superlists/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'lds!pp_37uu+)^e+wn08g0buy0ww2)-9j857^%p)1ghdew9uw0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
