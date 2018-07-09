763bb0cb4d6c5142a79305e921acb75da6f97cd9
ben-jacobson <ben_jacobson@live.com>
2018-07-10 08:26:07 +1000

    app now uses Django built in forms in all views. back to working state

--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import redirect, render
-from django.core.exceptions import ValidationError
+#from django.core.exceptions import ValidationError
 from lists.forms import ItemForm
 from lists.models import Item, List
 
@@ -10,7 +10,7 @@ def home_page(request):
     #    return redirect('/lists/the-only-list-in-the-world/')  # always re-direct a POST request, https://en.wikipedia.org/wiki/Post/Redirect/Get
     return render(request, 'home.html', {'form': ItemForm()})
 
-def new_list(request):
+'''def new_list(request):
     list_ = List.objects.create()
     item = Item.objects.create(text=request.POST['text'], list=list_)
     
@@ -24,22 +24,25 @@ def new_list(request):
 
     #return redirect(f'/lists/{list_.id}/')
     #return redirect('view_list', list_.id)
-    return redirect(list_)
+    return redirect(list_)'''
+
+def new_list(request):
+    form = ItemForm(data=request.POST)
+    if form.is_valid():
+        list_ = List.objects.create()
+        Item.objects.create(text=request.POST['text'], list=list_)
+        return redirect(list_)
+    else:
+        return render(request, 'home.html', {'form': form})
 
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
-    error = None
-
+    form = ItemForm()
     if request.method == 'POST':
-        try:
-            item = Item(text=request.POST['text'], list=list_)
-            item.full_clean()
-            item.save()
-            #return redirect(f'/lists/{list_.id}/')
+        form = ItemForm(data=request.POST)
+        if form.is_valid():
+            #form.save(for_list=list_)
+            Item.objects.create(text=request.POST['text'], list=list_)
             return redirect(list_)
-        except ValidationError:
-            list_.delete()
-            error = "You can't have an empty list item"
-            return render(request, 'home.html', {"error": error}) 
+    return render(request, 'list.html', {'list': list_, "form": form})
 
-    return render(request, 'list.html', {'list': list_, 'error': error})
