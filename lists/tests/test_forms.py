031b3e6a79ca14f4a34c12c5978dd12be13e7834
ben-jacobson <ben_jacobson@live.com>
2018-07-10 08:38:12 +1000

    Up to chapter 15

--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from lists.forms import EMPTY_ITEM_ERROR, ItemForm
+from lists.models import List, Item
 
 class ItemFormTest(TestCase):
 
@@ -12,3 +13,13 @@ class ItemFormTest(TestCase):
         form = ItemForm(data={'text': ''})
         self.assertFalse(form.is_valid())
         self.assertEqual(form.errors['text'], [EMPTY_ITEM_ERROR])
+
+    def test_form_handles_saving_to_a_list(self):
+        list_ = List.objects.create()
+        form = ItemForm(data={'text': 'do me'})
+        new_item = form.save(for_list=list_)
+        self.assertEqual(new_item, Item.objects.first())
+        self.assertEqual(new_item.text, 'do me')
+        self.assertEqual(new_item.list, list_)
+
+
--- lists/views.py
+++ lists/views.py
@@ -30,7 +30,8 @@ def new_list(request):
     form = ItemForm(data=request.POST)
     if form.is_valid():
         list_ = List.objects.create()
-        Item.objects.create(text=request.POST['text'], list=list_)
+        #Item.objects.create(text=request.POST['text'], list=list_)
+        form.save(for_list=list_)
         return redirect(list_)
     else:
         return render(request, 'home.html', {'form': form})
@@ -41,8 +42,8 @@ def view_list(request, list_id):
     if request.method == 'POST':
         form = ItemForm(data=request.POST)
         if form.is_valid():
-            #form.save(for_list=list_)
-            Item.objects.create(text=request.POST['text'], list=list_)
+            form.save(for_list=list_)
+            #Item.objects.create(text=request.POST['text'], list=list_)
             return redirect(list_)
     return render(request, 'list.html', {'list': list_, "form": form})
 
