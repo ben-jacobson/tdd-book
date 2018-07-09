687500dc02b9a90d52af3e0059c99bac8d93ba80
ben-jacobson <ben_jacobson@live.com>
2018-07-09 15:08:33 +1000

    enforce model validation in list view

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -26,7 +26,7 @@ class ItemValidationTest(FunctionalTest):
 
         # She receives a similar warning on the list page
         self.wait_for(lambda: self.assertEqual(
-            self.browser.find_element_by_css_selector('.has_error').text,
+            self.browser.find_element_by_css_selector('.has-error').text,
             "You can't have an empty list item"
         ))
 
--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -125,6 +125,18 @@ class NewListTest(TestCase):
         self.assertEqual(List.objects.count(), 0)
         self.assertEqual(Item.objects.count(), 0)
 
+    def test_validation_errors_end_up_on_lists_page(self):
+        list_ = List.objects.create()
+        response = self.client.post(
+            f'/lists/{list_.id}/',
+            data={'item_text': ''}
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'list.html')
+        expected_error = escape("You can't have an empty list item")
+        self.assertContains(response, expected_error)
+
+
 '''class SmokeTest(TestCase):
 
     def test_bad_maths(self):
