533d738894731ee8f2f18d50a818390c6459c12c
ben-jacobson <ben_jacobson@live.com>
2018-07-09 19:47:55 +1000

    rename all item input ids and names. still broken

--- lists/forms.py
+++ lists/forms.py
@@ -4,7 +4,7 @@ from lists.models import Item
 
 '''
 class ItemForm(forms.Form):
-    item_text = forms.CharField(
+    text = forms.CharField(
         widget=forms.fields.TextInput(attrs={
             'placeholder': 'Enter a to-do item',
             'class': 'form-control input-lg',
--- lists/views.py
+++ lists/views.py
@@ -6,13 +6,13 @@ from lists.models import Item, List
 # Create your views here.
 def home_page(request):
     #if request.method == 'POST':
-    #    Item.objects.create(text=request.POST['item_text'])
+    #    Item.objects.create(text=request.POST['text'])
     #    return redirect('/lists/the-only-list-in-the-world/')  # always re-direct a POST request, https://en.wikipedia.org/wiki/Post/Redirect/Get
     return render(request, 'home.html', {'form': ItemForm()})
 
 def new_list(request):
     list_ = List.objects.create()
-    item = Item.objects.create(text=request.POST['item_text'], list=list_)
+    item = Item.objects.create(text=request.POST['text'], list=list_)
     
     try:
         item.full_clean()
@@ -32,12 +32,14 @@ def view_list(request, list_id):
 
     if request.method == 'POST':
         try:
-            item = Item(text=request.POST['item_text'], list=list_)
+            item = Item(text=request.POST['text'], list=list_)
             item.full_clean()
             item.save()
             #return redirect(f'/lists/{list_.id}/')
             return redirect(list_)
         except ValidationError:
+            list_.delete()
             error = "You can't have an empty list item"
+            return render(request, 'home.html', {"error": error}) 
 
     return render(request, 'list.html', {'list': list_, 'error': error})
