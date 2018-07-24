09738d307872013c295e5e085ef79c2b1385dbd8
ben-jacobson <ben_jacobson@live.com>
2018-07-25 07:43:16 +1000

    minor file-save error breaking the build

--- functional_tests/test_my_lists.py
+++ functional_tests/test_my_lists.py
@@ -39,7 +39,7 @@ class MyListsTest(FunctionalTest):
         self.wait_for(
             lambda: self.browser.find_element_by_link_text('Reticulate splines')
         )
-        self.browser.find_element_by_link_text('Reticulate splines1').click()
+        self.browser.find_element_by_link_text('Reticulate splines').click()
         self.wait_for(
             lambda: self.assertEqual(self.browser.current_url, first_list_url)
         )
