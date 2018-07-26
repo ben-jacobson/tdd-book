b356467bf8aaaeb94b2eacea6d29223e06dcca3c
ben-jacobson <ben_jacobson@live.com>
2018-07-27 09:00:52 +1000

    Create page objects for list pages, use in sharing FT

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -2,7 +2,7 @@ from .server_tools import reset_database
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
-from selenium.webdriver.common.keys import Keys
+#from selenium.webdriver.common.keys import Keys
 import time, os 
 from datetime import datetime
 from .server_tools import create_session_on_server
@@ -89,60 +89,17 @@ class FunctionalTest(StaticLiveServerTestCase):                   # inherits fro
             timestamp=timestamp
         )
 
-    '''
-    def wait_for_row_in_list_table(self, row_text):
-        start_time = time.time()
-        while True:
-            try: 
-                table = self.browser.find_element_by_id('id_list_table')
-                rows = table.find_elements_by_tag_name('tr')
-                self.assertIn(row_text, [row.text for row in rows]) 
-                return
-            except (AssertionError, WebDriverException) as e:
-                if time.time() - start_time > MAX_WAIT:
-                    raise e
-                time.sleep(0.5) 
-    '''
-    
-    '''
-    def wait_for(self, fn):
-        start_time = time.time()
-        while True:
-            try: 
-                return fn()
-            except (AssertionError, WebDriverException) as e:
-                if time.time() - start_time > MAX_WAIT:
-                    raise e
-                time.sleep(0.5) 
-    '''
-
-    '''
-    def wait_to_be_logged_in(self, email):
-        self.wait_for(
-            lambda: self.browser.find_element_by_link_text('Log out')
-        )
-        navbar = self.browser.find_element_by_css_selector('.navbar')
-        self.assertIn(email, navbar.text)
-    '''
-
-    '''
-    def wait_to_be_logged_out(self, email):
-        self.wait_for(
-            lambda: self.browser.find_element_by_name('email')
-        )
-        navbar = self.browser.find_element_by_css_selector('.navbar')
-        self.assertNotIn(email, navbar.text)        
-    '''
-
-    def get_item_input_box(self):
+    '''def get_item_input_box(self):
         return self.browser.find_element_by_id('id_text')
+    '''
 
-    def add_list_item(self, item_text):
+    '''def add_list_item(self, item_text):
         num_rows = len(self.browser.find_elements_by_css_selector('#id_list_table tr'))
         self.get_item_input_box().send_keys(item_text)
         self.get_item_input_box().send_keys(Keys.ENTER)
         item_number = num_rows + 1
         self.wait_for_row_in_list_table(f'{item_number}: {item_text}')
+    '''
 
     # we've refactored some code to make use of decorators, here is the base function, below have been tagged 
 
@@ -150,12 +107,12 @@ class FunctionalTest(StaticLiveServerTestCase):                   # inherits fro
     def wait_for(self, fn):
         return fn()
 
-    @wait
+    '''@wait
     def wait_for_row_in_list_table(self, row_text):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertIn(row_text, [row.text for row in rows])
-
+    '''
 
     @wait
     def wait_to_be_logged_in(self, email):
